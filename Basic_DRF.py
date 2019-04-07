64780fed6a529c733077141d4bbd02687f5fa965
Eli Sherman <esherma@umich.edu>
2019-04-07 13:09:44 -0400

    updated Basic_DRF to be logically consistent with mesos master. next step is to run

--- Basic_DRF.py
+++ Basic_DRF.py
@@ -10,12 +10,19 @@ import numpy as np
 # In[1]:
 
 
-def dominantResourceFairness(resource_caps, resource_util, dominant_shares, utils):
+def getUserDemand(userID, request_dict):
+    return request_dict[userID][0]
+
+
+# In[2]:
+
+
+def dominantResourceFairness(resource_caps, resource_util, dominant_shares, utils, request_dict):
     #do argument sort to order dom shares ascending
     sorted_dom_shared = np.argsort(dominant_shares)
     #check each user to see if we can service their next request
     for i in sorted_dom_shares:
-        userDemand = getUserDemand(i)
+        userDemand = getUserDemand(i, request_dict)
         if not userDemand:
             #if this user doesn't have any current requests,
             #move to the next guy
@@ -30,14 +37,4 @@ def dominantResourceFairness(resource_caps, resource_util, dominant_shares, util
             return True, resource_caps, resource_util, dominant_shares, utils #return success and updated params
     #if no request can be serviced currently, return failure and the old parameters
     return False, resource_caps, resource_util, dominant_shares, utils
-
-
-# In[5]:
-
-
-numUsers = 10
-resources = np.array([1, 2, 3, 4, 5])
-resource_utilizations = np.zeros(5)
-userUtilization = np.zeros((numUsers, resources.shape[0]))
-userDomShares = np.zeros(numUsers)
 
