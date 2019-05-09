87346f5b96de87e015d8bd4351dcc7839fcaa1e1
ykl7 <yklal95@gmail.com>
2019-05-08 21:14:31 -0400

    more flask jobs

--- generate_flask_jobs.py
+++ generate_flask_jobs.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-f =open('flask-jobs.json', 'w+')
+f =open('flask-jobs2.json', 'w+')
 start_id = 3000
 
-for i in range(500):
+for i in range(1000):
     ram = str(np.random.randint(1, 5))
     cpu = str(np.random.randint(1, 4))
     fname = ['test12', 'test11', 'test13', 'test14', 'test15', 'test21', 'test23', 'test24', 'test25', 'test31', 'test33', 'test34', 'test35', 'test41', 'test43', 'test44', 'test45', 'test51', 'test53', 'test54', 'test55']
